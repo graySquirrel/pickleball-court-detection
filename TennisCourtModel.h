d95f579a01d851e6bded6aaf0dc37d7b0fa8436e
graySquirrel <fritzebner@gmail.com>
2022-11-17 18:56:16 -0500

    updated for pickleball, tested on macos

--- TennisCourtModel.h
+++ TennisCourtModel.h
@@ -13,30 +13,30 @@ class TennisCourtModel
 public:
   TennisCourtModel();
 
-  TennisCourtModel(const TennisCourtModel& o);
+  TennisCourtModel(const TennisCourtModel &o);
 
-  TennisCourtModel& operator=(const TennisCourtModel& o);
+  TennisCourtModel &operator=(const TennisCourtModel &o);
 
-  float fit(const LinePair& hLinePair, const LinePair& vLinePair, const cv::Mat& binaryImage,
-    const cv::Mat& rgbImage);
+  float fit(const LinePair &hLinePair, const LinePair &vLinePair, const cv::Mat &binaryImage,
+            const cv::Mat &rgbImage);
 
-  static std::vector<LinePair> getPossibleLinePairs(std::vector<Line>& lines);
+  static std::vector<LinePair> getPossibleLinePairs(std::vector<Line> &lines);
 
-  void drawModel(cv::Mat& image, cv::Scalar color=cv::Scalar(0, 255, 255));
+  void drawModel(cv::Mat &image, cv::Scalar color = cv::Scalar(0, 0, 255));
 
-  void writeToFile(const std::string& filename);
+  void writeToFile(const std::string &filename);
 
 private:
-  std::vector<cv::Point2f> getIntersectionPoints(const LinePair& hLinePair, const LinePair& vLinePair);
+  std::vector<cv::Point2f> getIntersectionPoints(const LinePair &hLinePair, const LinePair &vLinePair);
 
-  float evaluateModel(const std::vector<cv::Point2f>& courtPoints, const cv::Mat& binaryImage);
+  float evaluateModel(const std::vector<cv::Point2f> &courtPoints, const cv::Mat &binaryImage);
 
-  float computeScoreForLineSegment(const cv::Point2f& start, const cv::Point2f& end,
-    const cv::Mat& binaryImage);
+  float computeScoreForLineSegment(const cv::Point2f &start, const cv::Point2f &end,
+                                   const cv::Mat &binaryImage);
 
-  bool isInsideTheImage(float x, float y, const cv::Mat& image);
+  bool isInsideTheImage(float x, float y, const cv::Mat &image);
 
-  void drawModel(std::vector<cv::Point2f>& courtPoints, cv::Mat& image, cv::Scalar color=cv::Scalar(0, 255, 255));
+  void drawModel(std::vector<cv::Point2f> &courtPoints, cv::Mat &image, cv::Scalar color = cv::Scalar(0, 0, 255));
 
   std::vector<Line> hLines;
   std::vector<Line> vLines;
@@ -44,5 +44,4 @@ private:
   std::vector<LinePair> vLinePairs;
   std::vector<cv::Point2f> courtPoints;
   cv::Mat transformationMatrix;
-
 };
